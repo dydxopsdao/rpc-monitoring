aadbe7d26ff6aac3add9b44c81dbef0c7bff13ec
Dimitar Petkov <796902+2dpetkov@users.noreply.github.com>
2025-04-29 16:08:31 +0300

    fix:remove polkachu experimental endpoint

--- rpc_monitor/check_rpc.js
+++ rpc_monitor/check_rpc.js
@@ -5,7 +5,6 @@ const axios = require('axios');
 const PROVIDERS = {
     "dydx-ops-rpc.kingnodes.com": "kingnodes",
     "dydx-dao-rpc.polkachu.com": "polkachu",
-    "dydx-dao-rpc-new.polkachu.com": "polkachu-experimental",
     "dydx-dao-rpc.enigma-validator.com": "enigma"
 };
 
